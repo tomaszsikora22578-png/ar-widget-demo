66f27ebac15877da31c2e59f5e0323b8ed17eabe
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-11-10 12:26:03 +0100

    Update ar-widget.js

--- ar-widget.js
+++ ar-widget.js
@@ -1,5 +1,5 @@
 // Cloud Run API endpoint
-const API_URL = 'https://ar-widget-api-849496305543.europe-central2.run.app/';
+const API_URL = 'https://ar-widget-api-849496305543.europe-central2.run.app/widget/api';
         
 // TEST TOKEN (You need to replace this with a valid token if TEST_TOKEN_XYZ fails)
 const DEMO_TOKEN = 'TEST_TOKEN_XYZ'; 
